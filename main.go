cad9c6eced907f4770070aa917aee083f815a9e1
Gunnar Larsen <beachviking@gmail.com>
2024-10-10 08:06:22 +0200

    Cleanup

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 
+// may be usefull to look at binary dumps in the terminal:
+// od -h sidtune.dmp | less
+
 import (
-	"encoding/binary"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 )
 
 const MAX_INSTR uint16 = 0xFFFF
 
 func main() {
-	opt := SidOutputSettings{}
+	// opt := SidOutputSettings{}
+	opt := NewSidOutputSettings()
+	header := NewPSID()
 	var frame int = 0
 
 	// Parse arguments
 	opt.ParseArgs()
 
-	// Open the SID file
 	if len(flag.Args()) == 0 {
 		fmt.Println("Usage: go run main.go [options] <sidfile>")
 		os.Exit(1)
@@ -28,6 +30,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	// get file name of sid tune
 	sidName := flag.Arg(0)
 
 	// Try to open SID file
@@ -35,62 +38,26 @@ func main() {
 	check(err)
 	defer file.Close()
 
-	// Read interesting parts of the SID header
-	file.Seek(6, 0)
-	dataOffset := readWord(file)
-	loadAddress := readWord(file)
-	initAddress := readWord(file)
-	playAddress := readWord(file)
+	// Load PSID header
+	err = header.LoadPSIDHeader(file)
+	check(err)
 
-	file.Seek(int64(dataOffset), 0)
-	if loadAddress == 0 {
-		loadAddress = uint16(readByte(file)) | uint16(readByte(file))<<8
-	}
+	header.PrintPSIDVitals()
 
-	fmt.Printf("dataOffset:  0x%X\n", dataOffset)
-	fmt.Printf("loadAddress: 0x%X\n", loadAddress)
-	fmt.Printf("initAddress: 0x%X\n", initAddress)
-	fmt.Printf("playAddress: 0x%X\n", playAddress)
-
-	// Load the C64 data
-	filePos, fileErr := file.Seek(0, io.SeekCurrent)
-	check(fileErr)
-	loadPos := filePos
-	filePos, fileErr = file.Seek(0, io.SeekEnd)
-	check(fileErr)
-	loadEnd := uint16(filePos)
-	loadSize := uint16(loadEnd) - uint16(loadPos)
-	file.Seek(int64(loadPos), 0)
-
-	if loadSize+loadAddress >= 0x10000-1 {
-		panic("Error: SID data continues past end of C64 memory.")
-	}
-
-	memPos := uint16(loadAddress)
+	// Load PSID data into cpu memory
 	cpu := NewCpu()
-
-	for {
-		var b byte
-
-		fileErr = binary.Read(file, binary.LittleEndian, &b)
-
-		if fileErr == io.EOF {
-			break
-		}
-		cpu.Mem.StoreByte(memPos, b)
-		// fmt.Printf("Adr %04X val %02X\n", memPos, b)
-		memPos++
-	}
+	err = header.LoadPSIDData(cpu, file)
+	check(err)
 
 	// Print info and run initroutine
-	fmt.Printf("Load address: $%04X Init address: $%04X Play address: $%04X\n", loadAddress, initAddress, playAddress)
+	fmt.Printf("Load address: $%04X Init address: $%04X Play address: $%04X\n", header.LoadAddress, header.InitAddress, header.PlayAddress)
 	fmt.Printf("Calling initroutine with subtune %d\n", opt.Subtune)
 	cpu.Mem.StoreByte(0x01, 0x37)
-	InitCpu(cpu, initAddress, uint8(opt.Subtune), 0, 0)
+	Init(cpu, header.InitAddress, uint8(opt.Subtune), 0, 0)
 	instr := 0
 
-	for RunCpu(cpu) == 1 {
-		IncAtAddress(cpu, 0xD012)
+	for Run(cpu) == 1 {
+		IncrementValueAtAddress(cpu, 0xD012)
 		if (cpu.Mem.LoadByte(0xD012) == 0) || (((cpu.Mem.LoadByte(0xD011) & 0x80) != 0) && (cpu.Mem.LoadByte(0xd012) >= 0x38)) {
 			tmp := cpu.Mem.LoadByte(0xD011)
 			tmp ^= 0x80
@@ -105,27 +72,30 @@ func main() {
 		}
 	}
 
-	if playAddress == 0 {
+	if header.PlayAddress == 0 {
 		fmt.Println("Warning: SID has play address 0, reading from interrupt vector instead")
 		if cpu.Mem.LoadByte(0x01)&0x07 == 0x5 {
-			playAddress = uint16(cpu.Mem.LoadByte(0xFFFE)) | (uint16(cpu.Mem.LoadByte(0xFFFF)) << 8)
+			header.PlayAddress = uint16(cpu.Mem.LoadByte(0xFFFE)) | (uint16(cpu.Mem.LoadByte(0xFFFF)) << 8)
 		} else {
-			playAddress = uint16(cpu.Mem.LoadByte(0x314)) | (uint16(cpu.Mem.LoadByte(0x315)) << 8)
+			header.PlayAddress = uint16(cpu.Mem.LoadByte(0x314)) | (uint16(cpu.Mem.LoadByte(0x315)) << 8)
 		}
-		fmt.Printf("New play address is $%04X\n", playAddress)
+		fmt.Printf("New play address is $%04X\n", header.PlayAddress)
 	}
 
 	currentSid := NewSID()
 
 	// Create requested output struct type
-	screenSidReg := &ScreenOutputSidRegisters{Options: &opt, SidState: currentSid}
-	screenNotes := &ScreenOutputWithNotes{Options: &opt, SidState: currentSid}
+	screenSidReg := &ScreenOutputSidRegisters{Options: opt, SidState: currentSid}
+	screenNotes := &ScreenOutputWithNotes{Options: opt, SidState: currentSid}
+	fileSidDtDump := &BinFileRegistersAndDtDumps{Options: opt, SidState: currentSid, fileName: "sidtune.dmp"}
 
 	output := &ActiveDecoder{}
 
-	switch (opt.DecoderOutput) {
+	switch opt.DecoderOutput {
 	case 1:
 		output.SetOutput(screenSidReg)
+	case 4:
+		output.SetOutput(fileSidDtDump)
 	default:
 		output.SetOutput(screenNotes)
 	}
@@ -137,9 +107,9 @@ func main() {
 	for frame < opt.Firstframe+opt.Seconds*50 {
 		// Run the playroutine
 		instr = 0
-		InitCpu(cpu, playAddress, 0, 0, 0)
+		Init(cpu, header.PlayAddress, 0, 0, 0)
 
-		for RunCpu(cpu) == 1 {
+		for Run(cpu) == 1 {
 			instr += 1
 
 			if instr > int(MAX_INSTR) {
@@ -166,7 +136,4 @@ func main() {
 	}
 
 	output.PostProcess()
-
-	fmt.Println("Simulation done!")
-
 }
